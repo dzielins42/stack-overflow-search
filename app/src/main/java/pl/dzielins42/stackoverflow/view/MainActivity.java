f28a4ba1a6f91132850444d703025ab3caca0b60
dzielins42 <dzielins42@gmail.com>
2018-09-22 17:55:46 +0200

    Setup RecyclerView with Question list

--- app/src/main/java/pl/dzielins42/stackoverflow/view/MainActivity.java
+++ app/src/main/java/pl/dzielins42/stackoverflow/view/MainActivity.java
@@ -2,17 +2,23 @@ package pl.dzielins42.stackoverflow.view;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.hannesdorfmann.mosby3.mvi.MviActivity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import dagger.android.AndroidInjection;
 import io.reactivex.Flowable;
 import pl.dzielins42.stackoverflow.R;
+import pl.dzielins42.stackoverflow.database.model.Question;
 
 public class MainActivity
         extends MviActivity<MainView, MainPresenter>
@@ -23,11 +29,33 @@ public class MainActivity
     @Inject
     MainPresenter mMainPresenter;
 
+    @BindView(R.id.recycler)
+    RecyclerView mRecyclerView;
+
+    private QuestionAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        mAdapter = new QuestionAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+        List<Question> dummy = new ArrayList<>(10);
+        for(int i=0;i<10;i++){
+            dummy.add(
+                    Question.builder()
+                            .id(i)
+                            .title(String.valueOf(i))
+                            .authorDisplayName(String.valueOf(i))
+                            .answerCount(i)
+                            .build()
+            );
+        }
+        mAdapter.submitList(dummy);
     }
 
     @NonNull
---
+++ app/src/main/java/pl/dzielins42/stackoverflow/view/QuestionAdapter.java
@@ -0,0 +1,87 @@
+package pl.dzielins42.stackoverflow.view;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.recyclerview.extensions.ListAdapter;
+import android.support.v7.util.DiffUtil;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import pl.dzielins42.stackoverflow.R;
+import pl.dzielins42.stackoverflow.database.model.Question;
+
+public class QuestionAdapter extends ListAdapter<Question, QuestionAdapter.QuestionViewHolder> {
+
+    private final Context mContext;
+
+    protected QuestionAdapter(@NonNull Context context) {
+        super(new QuestionDiffUtilItemCallback());
+
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public QuestionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        final View view = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.item_question, parent, false
+        );
+        return new QuestionViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull QuestionViewHolder holder, int position) {
+        holder.bind(getItem(position));
+    }
+
+    public class QuestionViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.profile_image)
+        AppCompatImageView mProfileImage;
+        @BindView(R.id.author_header)
+        AppCompatTextView mAuthorHeader;
+        @BindView(R.id.title)
+        AppCompatTextView mTitle;
+        @BindView(R.id.answer_count)
+        AppCompatTextView mAnswerCount;
+
+        public QuestionViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bind(Question question) {
+            // TODO bind profile image
+            mAuthorHeader.setText(getFormattedAuthorHeader(question.getAuthorDisplayName()));
+            mTitle.setText(question.getTitle());
+            mAnswerCount.setText(String.valueOf(question.getAnswerCount()));
+        }
+
+        private Spanned getFormattedAuthorHeader(@NonNull String authorDisplayName) {
+            return Html.fromHtml(mContext.getString(R.string.author_header, authorDisplayName));
+        }
+    }
+
+    private static class QuestionDiffUtilItemCallback extends DiffUtil.ItemCallback<Question> {
+
+        @Override
+        public boolean areItemsTheSame(@NonNull Question question1, @NonNull Question question2) {
+            return question1.getId() == question2.getId();
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Question question1, @NonNull Question
+                question2) {
+            return question1.equals(question2);
+        }
+    }
+}
