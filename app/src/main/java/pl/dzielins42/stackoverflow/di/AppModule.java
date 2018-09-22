8618889692a0c8681ebdd17a5b9a29980ce61598
dzielins42 <dzielins42@gmail.com>
2018-09-22 22:36:49 +0200

    Provide hooks for so-api and database modules through AppModule

--- app/src/main/java/pl/dzielins42/stackoverflow/di/AppModule.java
+++ app/src/main/java/pl/dzielins42/stackoverflow/di/AppModule.java
@@ -1,12 +1,20 @@
 package pl.dzielins42.stackoverflow.di;
 
 import android.app.Application;
+import android.arch.persistence.room.Room;
 import android.content.Context;
 
+import javax.inject.Named;
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import pl.dzielins42.stackoverflow.api.StackOverflowService;
+import pl.dzielins42.stackoverflow.database.QuestionDao;
+import pl.dzielins42.stackoverflow.database.SODatabase;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @Module
 public class AppModule {
@@ -17,23 +25,37 @@ public class AppModule {
         return application;
     }
 
-    /*
     @Provides
     @Singleton
-    AppHelloService provideAppHelloService() {
-        return new AppHelloService();
+    @Named("stackoverflow")
+    Retrofit provideStackOverflowRetrofit() {
+        return new Retrofit.Builder()
+                .baseUrl("https://api.stackexchange.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build();
     }
 
     @Provides
     @Singleton
-    ActivityHelloService provideActivityHelloService() {
-        return new ActivityHelloService();
+    StackOverflowService provideStackOverflowService(@Named("stackoverflow") Retrofit retrofit) {
+        return retrofit.create(StackOverflowService.class);
     }
 
     @Provides
     @Singleton
-    FragmentHelloService provideFragmentHelloService() {
-        return new FragmentHelloService();
+    SODatabase provideDatabase(Context context) {
+        return Room.databaseBuilder(context, SODatabase.class, "stack-verflow-search-db")
+                .build();
+
     }
-    */
+
+    @Provides
+    @Singleton
+    QuestionDao provideQuestionDao(SODatabase db) {
+        QuestionDao questionDao = db.questionDao();
+        // Clear here so the old results are not displayed  at the start
+        questionDao.clear();
+        return questionDao;
+    }
 }
